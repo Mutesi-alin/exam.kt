9d0fa772ca17e1080f4fe36b300bf89aba62dfb4
Student <mutesi-alin>
2024-02-22 23:42:45 -0500

    added file

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var fullName="mutesi aline"
+    println(fullName)
+    var age=23
+    println(age)
+    var phoneNumber="0786766392"
+    println(phoneNumber)
+    var weight=23.9
+    println(weight)
+    var citizen="kenya"
+    println(citizen)
+
+
+}
